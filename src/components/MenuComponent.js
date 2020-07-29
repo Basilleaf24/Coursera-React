618478f014d0eb31f52d383bef7c41f39c47f2af
Prathibha R <prathi2kgrs@gmail.com>
2020-07-29 20:04:20 +0530

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import Dishdetail from './DishdetailComponent'
+
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-               selectedDish: null             
-            }
+        
             console.log('Menu Component constructor is invoked');
     }
 
@@ -15,18 +13,16 @@ class Menu extends Component {
         console.log('Menu Component componentDidMount is invoked');
     }
 
-    onDishSelect(dish){
-        this.setState({selectedDish: dish});
-    }
+    
 
    
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+              <div className="col-12 col-md-5 m-1">
+                <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+                  
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -43,7 +39,6 @@ class Menu extends Component {
                   <div className="row">
                         {menu}
                   </div>
-                  <Dishdetail dish={this.state.selectedDish}></Dishdetail>
                 </div>
               );
           }
