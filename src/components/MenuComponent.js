f50796cae35933151e37807938e9cb7ae39e7425
Prathibha R <prathi2kgrs@gmail.com>
2020-07-29 22:49:55 +0530

    Functional Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+    function RenderMenuItem({dish, onClick})
+    {
+      return(
+              <Card onClick={() => onClick(dish.id)}>                  
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+              </Card>
+      );
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-        
-            console.log('Menu Component constructor is invoked');
     }
 
-    componentDidMount(){
-        console.log('Menu Component componentDidMount is invoked');
-    }
+    const Menu=(props) =>{
+      const menu = props.dishes.map((dish) => {
+        return (
+          <div key={dish.id} className="col-12 col-md-5 m-1">
+            <RenderMenuItem dish={dish} onClick={props.onClick} />
+          </div>
+        );
+    });
 
-    
-
-   
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-                  
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
+    console.log('Menu Component render is invoked');
+     
+        return (
+            <div className="container">
+              <div className="row">
+                    {menu}
               </div>
-            );
-        });
-
-        console.log('Menu Component render is invoked');
-         
-            return (
-                <div className="container">
-                  <div className="row">
-                        {menu}
-                  </div>
-                </div>
-              );
-          }
-      }
-      
+            </div>
+          );
+    }
+    
       export default Menu;    
